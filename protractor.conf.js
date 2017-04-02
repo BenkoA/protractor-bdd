16861b31dea70db9da036644ca8dc9030f8336bc
Adrian Benko <benko.adrian@gmail.com>
2017-04-02 19:51:08 +0200

    trying to run webdriver-manager on Jenkins...

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,5 @@
 exports.config = {
-    seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+    //seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
     allScriptsTimeout: 100000,
     getPageTimeout: 100000,
 
